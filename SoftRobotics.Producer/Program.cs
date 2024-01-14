06423b889545e9e529073cb1e37e1f1cc19511b6
muhammedozdmr <muhammedhozdemir@gmail.com>
2024-01-14 23:02:48 +0300

    rabbitMQ

---
+++ SoftRobotics.Producer/Program.cs
@@ -0,0 +1,18 @@
+﻿using RabbitMQ.Client;
+
+var connectionFactory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    UserName = "SA",
+    Password = "Gofret-2608"
+};
+
+using (var connection = connectionFactory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare(queue: "RandomWord_Queue",
+        durable: false,
+        exclusive: false,
+        autoDelete: false,
+        arguments: null);
+}
