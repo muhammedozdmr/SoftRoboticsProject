8522dbb0827d3700d3dfe2331a7c8ba23260acd6
muhammedozdmr <muhammedhozdemir@gmail.com>
2024-01-16 10:29:21 +0300

    deleteProducer

--- SoftRobotics.Producer/Program.cs
+++ SoftRobotics.Producer/Program.cs
@@ -1,18 +1,1 @@
-﻿using RabbitMQ.Client;
-
-var connectionFactory = new ConnectionFactory()
-{
-    HostName = "localhost",
-    UserName = "SA",
-    Password = "Gofret-2608"
-};
-
-using (var connection = connectionFactory.CreateConnection())
-using (var channel = connection.CreateModel())
-{
-    channel.QueueDeclare(queue: "RandomWord_Queue",
-        durable: false,
-        exclusive: false,
-        autoDelete: false,
-        arguments: null);
-}
+﻿
