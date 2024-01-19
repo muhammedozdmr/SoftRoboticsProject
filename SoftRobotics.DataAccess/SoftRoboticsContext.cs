c9ffb120ad945de31e3c327b4c752af044a26636
muhammedozdmr <muhammedhozdemir@gmail.com>
2024-01-19 12:48:11 +0300

    LastUpgrade and Finish

--- SoftRobotics.DataAccess/SoftRoboticsContext.cs
+++ SoftRobotics.DataAccess/SoftRoboticsContext.cs
@@ -12,7 +12,6 @@ namespace SoftRobotics.DataAccess
     public class SoftRoboticsContext : DbContext
     {
         private const string ConnectionString = "Server = localhost; Database =  SoftRoboticsDb; User Id='.'; Password=''; Integrated Security = true;";
-        //private const string ConnectionString = "Server=localhost;Database=dbsoftrobotics;User Id='SA'; Password='Gofret-2608'; Integrated Security=false; Trusted_Connection=false";
 
         public DbSet<RandomWord> RandomWords { get; set; }
 
---
+++ SoftRobotics.RepositoryAPI/Controllers/APIRepositoryController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using SoftRobotics.Business;
+using SoftRobotics.Business.Repository;
+using SoftRobotics.DataAccess;
+using SoftRobotics.Dto;
+
+namespace SoftRobotics.RepositoryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class APIRepositoryController : Controller
+    {
+        private readonly SoftRoboticsContext _context = new SoftRoboticsContext();
+        private readonly RabbitRepository _repository = new RabbitRepository(dbContext: _context);
+        private IConnection _connection;
+        [HttpGet("GetRabbitRepository")]
+        public IActionResult GetRabbitRepository()
+        {
+            _repository.DirectExchange();
+            return Ok();
+        }
+        [HttpGet("IndexRepository")]
+        public IActionResult IndexRepository()
+        {
+            var randomwords = _repository.GetAll();
+            return Ok(randomwords);
+        }
+        public IActionResult Delete(RandomWordDto model)
+        {
+            var commandResult = _repository.Delete(model);
+            if (commandResult.IsSuccess)
+            {
+                return Ok(commandResult);
+            }
+            else
+            {
+                return NotFound(commandResult);
+            }
+        }
+    }
+}
